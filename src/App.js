5a415dc4caef52a3b996017c12b410acdb64dd47
cupokyle <kyleforsythcomedy@gmail.com>
2023-07-23 21:49:39 -0400

    add more

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import logo from './logo.png';
 import './App.css';
 
 const App = () => {
@@ -10,13 +11,24 @@ const App = () => {
   const [questions, setQuestions] = useState([]);  // To store the questions returned from the API
   const [responseString, setResponseString] = useState('');  // To store the environment description
   const [choices, setChoices] = useState([]);  // To store the player choices
+  const [isLoading, setIsLoading] = useState(false);
+
+  const colors = [
+    '#0D1B2A', '#1B263B', '#415A77', '#778DA9', '#E0E1DD', // Variations of dark blue
+    '#1C2321', '#344E41', '#4A7358', '#90A955', '#BAC1B8', // Variations of dark green
+    '#30011E', '#400123', '#610134', '#7C013A', '#951141', // Variations of dark purple
+    '#321325', '#522546', '#6B3A67', '#8F5788', '#B678AA', // More variations of dark purple
+    '#2B0404', '#3E0606', '#551111', '#8A2929', '#AD4646'  // Variations of dark red
+  ];
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setIsLoading(true);  // set loading to true
 
     let data = {
       'messages': [{'role':'system', 'content': text}],
     };
+
 
     if (turnCounter === 0) {
       data = {
@@ -34,7 +46,7 @@ const App = () => {
       });
 
       const systemContent = turnCounter === 1
-      ? 'Now it\'s time for character creation. Please return a new array of 8 questions but this time, they should be pivotal questions when creating the player character. Keep the questions under 15 words in length, and ask for specific character details that would correspond to a Role Playing game in this world. We want the player to feel like they\'ve built a great, original character, so ensure that they get to have some fun with it. Keep the yes-or-no questions to a minimum.'
+      ? 'Now is the time for character creation. please return a JavaScript array of exactly 8 questions that would help you develop a unique character based on the answers. Some questions should be basic and others pivotal questions that would help develop a character. Keep the questions under 12 words in length. Keep the yes-or-no questions to a minimum.'
       : turnCounter === 2
         ? `You are now the game's Dungeon Master. Please think of a fun and original Dungeons & Dragons style campaign that takes place in the world we just created, with the player being the character we jsut created. Please respond as the Dungeon Master beginning the game. Each turn you should format your response in this exact way: a JSON object with two key:value pairs. One key is called "responseString" and contains a string that is a fairly detailed description of the player's environment or situation. The other key is called "choices" which contains an array of 3 actions the player can complete. Each action should be concise, and no more than 12 words.`
         : '';
@@ -67,10 +79,13 @@ const App = () => {
       setTurnCounter(turnCounter + 1);
     } catch (error) {
       console.error(error);
+    } finally {
+      setIsLoading(false); // set loading back to false
     }
   };
 
   const handleChoice = async (choice) => {
+    setIsLoading(true);  // set loading to true
     const data = {
       'messages': [
         { 'role': 'system', 'content': responseString },
@@ -93,6 +108,8 @@ const App = () => {
       setChoices(choicesArray);
     } catch (error) {
       console.error(error);
+    } finally {
+      setIsLoading(false); // set loading back to false
     }
   };
   
@@ -108,54 +125,67 @@ const App = () => {
 
 return (
   <div className="App">
-    <h1>Kyle Game</h1>
-    {turnCounter >= 0 && turnCounter <= 2 && (
-      <form onSubmit={handleSubmit}>
-        {turnCounter === 0 && (
-          <>
-            <p>Please input a prompt</p>
-            <input
-              type="text"
-              className="input-field"
-              value={text}
-              onChange={e => setText(e.target.value)}
-            />
-          </>
-        )}
-        {(turnCounter >= 1 && turnCounter <= 3) && (
-          <>
-            <h2>{turnCounter === 1 ? 'World Creator' : turnCounter === 2 ? 'Character Creator' : 'Game Begins'}</h2>
-            {formInputs.map((inputValue, index) => (
-              <div key={index} className="form-group">
-                <label>{questions[index]}</label>
+    {isLoading ? (
+      <div className="loading">Loading...</div>  // the 'loading' class will center the text
+    ) : (
+      <>
+      <>
+        <img src={logo} alt="Logo" className="logo" />
+        <h1>Kyle Game</h1>
+      </>
+        {turnCounter >= 0 && turnCounter <= 2 && (
+          <form onSubmit={handleSubmit}>
+            {turnCounter === 0 && (
+              <>
+                <p>Please input a prompt</p>
                 <input
-                  className="input-field"
                   type="text"
-                  value={inputValue}
-                  onChange={e => handleChange(e, index)}
+                  className="input-field"
+                  value={text}
+                  onChange={e => setText(e.target.value)}
                 />
-              </div>
-            ))}
-          </>
+              </>
+            )}
+            {(turnCounter >= 1 && turnCounter <= 3) && (
+              <>
+                <h2>{turnCounter === 1 ? 'World Creator' : turnCounter === 2 ? 'Character Creator' : 'Game Begins'}</h2>
+                {formInputs.map((inputValue, index) => (
+                  <div key={index} className="form-group">
+                    <label>{questions[index]}</label>
+                    <input
+                      className="input-field"
+                      type="text"
+                      value={inputValue}
+                      onChange={e => handleChange(e, index)}
+                    />
+                  </div>
+                ))}
+              </>
+            )}
+            <button type="submit">Submit</button>
+          </form>
         )}
-        <button type="submit">Submit</button>
-      </form>
-    )}
 
-    {turnCounter >= 2 && (
-      <div>
-        <p>{responseString}</p>
-        {choices.map((choice, index) => (
-  <button key={index} onClick={() => handleChoice(choice)}>
-    {choice}
-  </button>
-))}
-      </div>
+        {turnCounter >= 2 && (
+          <div>
+            <p>{responseString}</p>
+            {choices.map((choice, index) => (
+              <button 
+                key={index} 
+                onClick={() => handleChoice(choice)}
+                style={{backgroundColor: colors[index % colors.length]}}
+                className="choice-button" /* Here we use our new class */
+              >
+                {choice}
+              </button>
+            ))}
+          </div>
+        )}
+      </>
     )}
   </div>
 );
-
-        }
+            }
 
 
 export default App;
